28ce479d74cd6e379b0cde7f0a27e1fabe37af6e
ianroberts131 <ianroberts131@gmail.com>
2016-05-20 13:39:59 +0000

    Authentication for RSpec

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe TopicsController, type: :controller do
 
   describe "GET index" do
     it "returns http success" do
+      sign_in(my_user)
       get :index
       expect(response).to have_http_status(:success)
     end
