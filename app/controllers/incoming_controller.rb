a6461671f9478dd72744608488382a27125be001
ianroberts131 <ianroberts131@gmail.com>
2016-06-06 14:14:38 +0000

    fix mailgun

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -1,5 +1,6 @@
 class IncomingController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
+  skip_before_action :authenticate_user!, only: [:create]
   
   def create
     puts "INCOMING PARAMS HERE: #{params}"
