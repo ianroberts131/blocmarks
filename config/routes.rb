26652b625d853f3c4ae6d92ee972779e6cfa69cb
ianroberts131 <ianroberts131@gmail.com>
2016-06-13 02:41:16 +0000

    Create user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do 
+  get 'users/show'
+
   get 'likes/index'
 
   devise_for :users
@@ -6,13 +8,14 @@ Rails.application.routes.draw do
   post :incoming, to: 'incoming#create'
   
   authenticated :user do
+    resources :users, only: [:show]
     resources :topics do
       resources :bookmarks, except: [:index]
     end
     resources :bookmarks, except: [:index] do
       resources :likes, only: [:index, :create, :destroy]
     end
-    root to: "topics#index", as: :authenticated_root
+    root to: "users#show", as: :authenticated_root
   end
   root 'welcome#home'
 end
